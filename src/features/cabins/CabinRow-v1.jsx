42399ae4b84bf7977459f7ca3817d868261cc4cf
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-04-23 18:53:11 +0700

    Update : reusable context menu

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
-
+import { formatCurrency } from '../../utils/helpers';
+// import { useMutation, useQueryClient } from '@tanstack/react-query';
+// import { deleteCabin } from '../../services/apiCabins';
+// import { toast } from 'react-hot-toast';
+import Button from '../../ui/Button';
 import CreateCabinForm from './CreateCabinForm';
 import { useDeleteCabin } from './useDeleteCabin';
-import { formatCurrency } from '../../utils/helpers';
 import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 import { useCreateCabin } from './useCreateCabin';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
 import Table from '../../ui/Table';
-import Menus from '../../ui/Menus';
 
 // const TableRow = styled.div`
 //   display: grid;
@@ -24,7 +26,7 @@ import Menus from '../../ui/Menus';
 
 const Img = styled.img`
   display: block;
-  width: 6.4rem;
+  width: 10rem;
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
@@ -57,7 +59,7 @@ function CabinRow({ cabin }) {
 
   function handleDuplicate() {
     createCabin({
-      name: `Copy of ${name}`,
+      name: `copy of ${name}`,
       maxCapacity,
       regularPrice,
       discount,
@@ -74,32 +76,29 @@ function CabinRow({ cabin }) {
       <Price>{formatCurrency(regularPrice)}</Price>
       {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
       <div>
+        <Button size="small" disabled={isCreating} onClick={handleDuplicate}>
+          <HiSquare2Stack />
+        </Button>
+
+        {/* button menu biasa tidak toggle */}
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
+          <Modal.Open opens="edit">
+            <Button size="small" variation="warning">
+              <HiPencil />
+            </Button>
+          </Modal.Open>
+          <Modal.Window name="edit">
+            <CreateCabinForm cabinToEdit={cabin} />
+          </Modal.Window>
 
-            <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
-                Duplicate
-              </Menus.Button>
-
-              <Modal.Open opens="edit">
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
-
-              <Modal.Open opens="delete">
-                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-              </Modal.Open>
-            </Menus.List>
-
-            <Modal.Window name="edit">
-              <CreateCabinForm cabinToEdit={cabin} />
-            </Modal.Window>
-
-            <Modal.Window name="delete">
-              <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)} />
-            </Modal.Window>
-          </Menus.Menu>
+          <Modal.Open opens="delete">
+            <Button variation="danger" size="small" disabled={isDeleting}>
+              <HiTrash />
+            </Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)} />
+          </Modal.Window>
         </Modal>
       </div>
     </Table.Row>
