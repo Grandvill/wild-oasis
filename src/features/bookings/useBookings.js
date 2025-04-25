e83c404e64b3d313fb93a6efb0106a4ed839db62
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-04-25 13:59:46 +0700

    Add : filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // 1. FILTER
+  const filterValue = searchParams.get('status');
+  const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
@@ -9,7 +16,7 @@ export function useBookings() {
   } = useQuery({
     queryKey: ['cabins'],
     // (query function) — untuk GET/fetch data
-    queryFn: getBookings,
+    queryFn: () => getBookings({ filter }),
   });
 
   return { bookings, isLoading, error };
