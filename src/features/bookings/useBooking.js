d9bb0b2fb4e1485717f67eb1a85d32810052e1ef
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-04-28 16:04:44 +0700

    Add : Checking in a booking

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -10,7 +10,7 @@ export function useBooking() {
     data: booking,
     error,
   } = useQuery({
-    queryKey: ['booking'],
+    queryKey: ['booking', bookingId],
     // (query function) — untuk GET/fetch data
     queryFn: () => getBooking(bookingId),
     retry: false,
