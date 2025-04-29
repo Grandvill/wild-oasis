6529e3fdb396c65a2bce52c25d83862934c8fd3b
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-04-29 14:57:36 +0700

    Add : authentication user login with supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -3,7 +3,7 @@ import Form from '../../ui/Form';
 import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
-import FormRow from '../../ui/formRow';
+import FormRow from '../../ui/FormRow';
 import { useForm } from 'react-hook-form';
 import { useCreateCabin } from './useCreateCabin';
 import { useEditCabin } from './useEditCabin';
---
+++ src/services/app.js
@@ -0,0 +1,2 @@
+const myName = 'Zahidan';
+console.log(myName);
