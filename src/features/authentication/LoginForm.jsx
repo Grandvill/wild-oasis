362758d77f862b409caab1eaea0ed4b0a7d566a2
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-05-06 12:55:44 +0700

    Fix : login form and button list at cabin table

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('zahidan23@gmail.com');
-  const [password, setPassword] = useState('Frayrandall456#');
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
